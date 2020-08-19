83afac4df15eb9805740ccd29c283d053fc5b949
Adnan Adil <adnanadil529@gmail.com>
2020-08-19 13:35:58 +0300

    removing the brackets

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,32 +3,34 @@ import './CounrtyCard.css'
 
 const CounrtyCard = (props) => {
 
-    {if (props.sorted){
-        return(
-            <div className = "Card">
-                <img src = {props.link} alt=""/>
-                <p className = "countryName">{props.countryName}</p>
-                <p className = "casesToday">{`New Confirmed Cases: ${props.casesToday}`}</p>
-                <p className = "totalCase">{`Total Confirmed Cases: ${props.totalCase}`}</p>
-                <p className = "TotalDeaths">{`Total Deaths: ${props.TotalDeaths}`}</p>
-                <p className = "date">{`Updated: ${props.date}`}</p>
-                <p className = "rank">{`rank: ${props.rank}`}</p>
-            </div>
-        );
-    }else {
-        return(
-            <div className = "Card">
-                <img src = {props.link} alt=""/>
-                <p className = "countryName">{props.countryName}</p>
-                <p className = "casesToday">{`New Confirmed Cases: ${props.casesToday}`}</p>
-                <p className = "totalCase">{`Total Confirmed Cases: ${props.totalCase}`}</p>
-                <p className = "TotalDeaths">{`Total Deaths: ${props.TotalDeaths}`}</p>
-                <p className = "date">{`Updated: ${props.date}`}</p>
-            </div>
-        );
-    }
+    
+        if (props.sorted){
+            return(
+                <div className = "Card">
+                    <img src = {props.link} alt=""/>
+                    <p className = "countryName">{props.countryName}</p>
+                    <p className = "casesToday">{`New Confirmed Cases: ${props.casesToday}`}</p>
+                    <p className = "totalCase">{`Total Confirmed Cases: ${props.totalCase}`}</p>
+                    <p className = "TotalDeaths">{`Total Deaths: ${props.TotalDeaths}`}</p>
+                    <p className = "date">{`Updated: ${props.date}`}</p>
+                    <p className = "rank">{`rank: ${props.rank}`}</p>
+                </div>
+            );
+        }else {
+            return(
+                <div className = "Card">
+                    <img src = {props.link} alt=""/>
+                    <p className = "countryName">{props.countryName}</p>
+                    <p className = "casesToday">{`New Confirmed Cases: ${props.casesToday}`}</p>
+                    <p className = "totalCase">{`Total Confirmed Cases: ${props.totalCase}`}</p>
+                    <p className = "TotalDeaths">{`Total Deaths: ${props.TotalDeaths}`}</p>
+                    <p className = "date">{`Updated: ${props.date}`}</p>
+                </div>
+            );
+    
+    //
 
-}
+    }
     
 }
 
